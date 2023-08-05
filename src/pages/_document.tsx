3a5bd1f93bdbe97da09168efe4e721c70b22cf4d
Alexey Pudnikov <alexey.pudnikov@yandex.ru>
2023-08-05 16:34:08 +0000

    api model

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Borel&display=swap"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import { fork, allSettled, serialize } from "effector";
-import { $authToken, appInited, requestFx } from "../../api";
+import { useUnit } from "effector-react";
+import { $authToken, appInited, getFactFx, $fact } from "../../api";
+import Image from "next/image";
 
-const Page = () => <p>page</p>;
+const Page = () => {
+  const { fact } = useUnit({ fact: $fact });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        maxWidth: 500,
+        padding: 20,
+        font: "22px Borel, cursive",
+        margin: 'auto'
+      }}
+    >
+      <Image
+        src="https://cdn-icons-png.flaticon.com/512/112/112639.png"
+        width={100}
+        height={100}
+        alt="Stare cat"
+      />
+      <p>{fact}</p>
+    </div>
+  );
+};
 
 export default Page;
 
@@ -11,7 +37,8 @@ export async function getServerSideProps() {
   });
 
   await allSettled(appInited, { scope });
-  await allSettled(requestFx, { scope });
+  await allSettled(getFactFx, { scope });
+
   return {
     props: {
       values: serialize(scope),
